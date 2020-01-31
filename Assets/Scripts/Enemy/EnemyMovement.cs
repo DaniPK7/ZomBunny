61ded63083f4be6a0c94b331f5f20d4a1d105f2f
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2020-01-31 15:48:21 +0100

    ej2

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -9,6 +9,9 @@ public class EnemyMovement : MonoBehaviour
     EnemyHealth enemyHealth;        // Reference to this enemy's health.
     NavMeshAgent nav;               // Reference to the nav mesh agent.
 
+    Animator EnemyAnim;
+    bool isInRange;
+    public float range = 150f;
 
     void Awake()
     {
@@ -18,12 +21,27 @@ public class EnemyMovement : MonoBehaviour
         playerHealth = player.GetComponent<PlayerHealth>();
         enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent<NavMeshAgent>();
+
+        EnemyAnim= GetComponent<Animator>();
     }
 
 
     void Update()
     {
-        nav.SetDestination(player.position);
+        float dist = Vector3.Distance(player.position, transform.position);
+        print(dist);
+        if (dist <= range) 
+        {
+            isInRange = true;
+            nav.SetDestination(player.position);
+        }
+        else 
+        { 
+            isInRange = false; 
+        }
+
+        EnemyAnim.SetBool("InRange", isInRange);
+
         // If the enemy and the player have health left...
 
         /*if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
@@ -37,5 +55,6 @@ public class EnemyMovement : MonoBehaviour
             // ... disable the nav mesh agent.
             nav.enabled = false;
         }*/
+
     }
 }
